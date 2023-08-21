9b32dfe1cdfc05d2e11a72dff5122afed4510e61
Niclas0923 <736012843@qq.com>
2023-08-21 22:30:36 +0800

    优化更新流程

--- page/src/App.vue
+++ page/src/App.vue
@@ -3,19 +3,11 @@ import {onMounted} from "vue";
 import axios from "axios";
 import {useValueStore} from "./store/useValueStore.js";
 
-// 引入list数据用于导入
+// 引入list来更新数据
 const list = useValueStore()
 
 onMounted(()=>{
-  axios.get("/data").then(
-      data=>{
-        if (data.data) list.list = data.data
-      },
-      err=>{
-        console.log(err)
-        alert("网络访问失败")
-      }
-  )
+  list.updateList()
 })
 </script>
 
--- page/src/store/useValueStore.js
+++ page/src/store/useValueStore.js
@@ -1,7 +1,23 @@
 import {defineStore} from "pinia";
 import {reactive} from "vue";
+import axios from "axios";
 
 export const useValueStore = defineStore("list",()=>{
-    const list = reactive([])
-    return {list}
+    // 列表信息
+    let list = reactive([])
+
+    // 更新列表信息
+    function updateList(){
+        axios.get("/data").then(
+            data=>{
+                if (data.data) list = data.data
+            },
+            err=>{
+                console.log(err)
+                alert("网络访问失败")
+            }
+        )
+    }
+
+    return {list,updateList}
 })

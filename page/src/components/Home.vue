62f4c4e0404553cd01c10475246b7441dcfcf9a1
Niclas0923 <736012843@qq.com>
2023-08-24 15:39:30 +0800

    设置router并且简单制作了home页面

---
+++ page/src/components/Home.vue
@@ -0,0 +1,41 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-6 cad">
+        <div class="cad-in text-center cardRollCallTool">
+          <h4 class="mb-0">点名工具</h4>
+        </div>
+      </div>
+      <div class="col-6 cad">
+        <div class="cad-in text-center cardWeChatRelay">
+          <h4 class="mb-0">微信接龙</h4>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.cad{
+  padding: 20px 40px;
+}
+.cad-in{
+  padding: 40px 10px;
+  border-radius: 15px;
+}
+.cardRollCallTool{
+  background-color: rgb(201,202,201);
+  box-shadow: 1px 1px 8px gray;
+  //text-shadow: 0 0 10px #dadada;
+}
+.cardWeChatRelay{
+  background-color: rgb(9,206,102);
+  box-shadow: 1px 1px 10px rgb(9, 208, 103);
+  color: white;
+  text-shadow: 0 0 4px gray;
+}
+</style>
--- page/src/router/index.js
+++ page/src/router/index.js
@@ -1,19 +1,20 @@
 import * as VueRouter from 'vue-router'
+import Home from "../components/home.vue";
 
 export default VueRouter.createRouter({
     // 设置使用 hash
     history: VueRouter.createWebHashHistory(),
     routes:[
         // 一级路由
-        // {
-        //     path: '/',
-        //     // 直接跳转 /home
-        //     redirect: '/home'
-        // },
-        // // 二级路由
-        // {
-        //     path:"/home",
-        //     component:,
-        // },
+        {
+            path: '/',
+            // 直接跳转 /home
+            redirect: '/home'
+        },
+        // 二级路由
+        {
+            path:"/home",
+            component:Home,
+        },
     ]
 })

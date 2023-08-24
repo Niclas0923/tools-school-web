3bbb26361a445e8eea72a1ba6a9ab54faae2b5f5
Niclas0923 <736012843@qq.com>
2023-08-25 00:45:57 +0800

    梳理数据结构

--- page/src/store/useValueStore.js
+++ page/src/store/useValueStore.js
@@ -1,16 +1,34 @@
 import {defineStore} from "pinia";
-import {reactive} from "vue";
+import {computed,shallowRef} from "vue";
 import axios from "axios";
 
-export const useValueStore = defineStore("list",()=>{
+export const useValueStore = defineStore("val",()=>{
     // 列表信息
-    let list = reactive([])
+    let list = shallowRef([])
+
+    // 点名使用的miniId与姓名的数组
+    const miniIdAndNames = computed(()=>{
+        const l = []
+        list.value.forEach(v=>{
+            l.push({name:v["name"], miniId:v["id"].slice(-2)})
+        })
+        return l
+    })
+
+    // 数据配合使用的信息数组
+    const allValueList = computed(()=>{
+        const l = []
+        list.value.forEach(v=>{
+            l.push([v["name"],v["id"],...v["tags"]])
+        })
+        return l
+    })
 
     // 更新列表信息
     function updateList(){
         axios.get("/data").then(
             data=>{
-                if (data.data) list = data.data
+                if (data.data) list.value = data.data
             },
             err=>{
                 console.log(err)
@@ -19,5 +37,5 @@ export const useValueStore = defineStore("list",()=>{
         )
     }
 
-    return {list,updateList}
+    return {list,miniIdAndNames,allValueList,updateList}
 })

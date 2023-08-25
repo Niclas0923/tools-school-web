36de5e7b63aaa824e7b20a7f3457fc8c155c57e3
Niclas0923 <736012843@qq.com>
2023-08-25 21:20:40 +0800

    完成本地保存功能

--- page/src/store/useRollCallListStore.js
+++ page/src/store/useRollCallListStore.js
@@ -1,5 +1,5 @@
 import {defineStore} from "pinia";
-import {reactive} from "vue";
+import {reactive, toRaw, watch} from "vue";
 
 export const useRollCallListStore = defineStore("rollCallList",()=>{
     const list = reactive({
@@ -13,20 +13,29 @@ export const useRollCallListStore = defineStore("rollCallList",()=>{
     function initialization(ids){
         list.allId = [...ids.value]
         // 尝试读取localStorage
-        // const val = localStorage.getItem("RollCall-home")
-        // if (val){
-        //     list.home = JSON.parse(val)
-        // }
-        console.log(list.allOn.home)
+        const val = localStorage.getItem("RollCall")
+        if (val){
+            list.allOn = JSON.parse(val)
+        }else{
+            safe()
+        }
+        // console.log(list.allOn.home)
+    }
+
+    // 保存
+    function safe(){
+        localStorage.setItem("RollCall",JSON.stringify(toRaw(list.allOn)))
     }
 
     function onClick(i){
         list.allOn.home.splice(list.allOn.home.indexOf(i),1)
+        safe()
     }
 
     function downClick(i){
         list.allOn.home.push(Number(i))
         list.allOn.home.sort((a, b)=> a - b)
+        safe()
     }
 
     return {list,initialization,onClick,downClick}
